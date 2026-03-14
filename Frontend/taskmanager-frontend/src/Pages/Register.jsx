c4c44d6ff364c6d877fff9648881b3bc5a721326
Umaya <umayadharsh@gmail.com>
2026-03-14 18:27:24 +0530

    fix file case for deployment5

--- Frontend/taskmanager-frontend/src/Pages/Register.jsx
+++ Frontend/taskmanager-frontend/src/Pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import api from "../utils/api";
+import api from "../services/api.js";
 
 export default function Register() {
   const navigate = useNavigate();
