1d666208247edfceb888bfb0a4a3563176dc62f3
Umaya <umayadharsh@gmail.com>
2026-03-14 18:09:38 +0530

    fix file case for deployment1

--- Frontend/taskmanager-frontend/src/App.jsx
+++ Frontend/taskmanager-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard,jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
 
 function App() {
